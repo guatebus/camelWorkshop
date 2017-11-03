730e7bfe97236c12d172873648b14ef152d872db
Alejandro Bustamante <alejandro.bustamante@scmspain.com>
2017-11-03 13:37:45 +0100

    initial commit - basic gradle camel application

---
+++ src/main/java/camel/workshop/Application.java
@@ -0,0 +1,10 @@
+package camel.workshop;
+
+import org.apache.camel.main.Main;
+
+public class Application {
+    public static void main(String[] args) throws Exception{
+        Main main = new Main();
+        main.run(args);
+    }
+}
